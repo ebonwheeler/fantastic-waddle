0228aa53973c5eec773593fd01d6072282ee3601
Philip Goh <philip.wj.goh@gmail.com>
2017-07-29 07:15:20 +0100

    Adding logging to the open weathermap interface

--- utils/OpenWeatherMapInterface.js
+++ utils/OpenWeatherMapInterface.js
@@ -1,5 +1,7 @@
 var request = require("request");
 var Q = require("q");
+var log4js = require("log4js");
+var logger = log4js.getLogger("OpenWeatherMapInterface");
 
 function OpenWeatherMapInterface(options) {
   this.apiKey = options.api_key;
@@ -8,6 +10,8 @@ function OpenWeatherMapInterface(options) {
 OpenWeatherMapInterface.prototype.queryPostCode = function(postCode, countryCode) {
   var url = "https://api.openweathermap.org/data/2.5/weather?zip=" + postCode + "," + countryCode "&APPID=" + this.apiKey;
   var deferred = Q.defer();
+
+  logger.debug("Making request to: ", url);
   request.get(url, function(err, res, body) {
     if(err) {
       return deferred.reject(err);
