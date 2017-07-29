59b4d12e8abf9d88340fcf27b6746ef7031ca9f1
Philip Goh <philip.wj.goh@gmail.com>
2017-07-29 11:52:21 +0100

    Use HTTP as HTTPS is paid for.

--- utils/OpenWeatherMapInterface.js
+++ utils/OpenWeatherMapInterface.js
@@ -8,7 +8,8 @@ function OpenWeatherMapInterface(options) {
 }
 
 OpenWeatherMapInterface.prototype.queryPostCode = function(postCode, countryCode) {
-  var url = "https://api.openweathermap.org/data/2.5/weather?zip=" + postCode + "," + countryCode + "&APPID=" + this.apiKey;
+  // Use HTTP instead of HTTPS because HTTPS is paid for
+  var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + postCode + "," + countryCode + "&APPID=" + this.apiKey;
   var deferred = Q.defer();
 
   logger.debug("Making request to: ", url);
